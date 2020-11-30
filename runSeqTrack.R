2643b9e7188e826b6fe62f6227802102515959a8
mbjorner <37785738+mbjorner@users.noreply.github.com>
2020-11-29 19:13:09 -0600

    update seqTrack.R
    added actual seqTrack alg

--- runSeqTrack.R
+++ runSeqTrack.R
@@ -1,3 +1,41 @@
 library(adegenet)
 library(ape)
 
+## Sources:
+# dist.dna function
+# https://www.rdocumentation.org/packages/ape/versions/3.0-9/topics/dist.dna
+
+# seqTrack algorithm for reconstructing genealogies:
+# https://www.rdocumentation.org/packages/adegenet/versions/2.0.1/topics/seqTrack
+
+## load data
+dat = read.csv("H1N1_time_location_data.csv", header = T)       # package = "adegenet" unused.
+ha  = read.dna("H1N1_HA_sequences.txt", format="fa")
+na  = read.dna("H1N1_NA_sequences.txt", format="fa")
+
+## computing nucleotide distances
+# to calculate raw pairwise genetic distances between isolates,
+# ape package can be used (dist.dna)
+nbNucleotide = ncol(as.matrix(ha)) + ncol(as.matrix(na))
+distances    = dist.dna(ha, model = "raw")*ncol(as.matrix(ha)) +
+               dist.dna(na, model = "raw")*ncol(as.matrix(na))
+distances    = round(as.matrix(distances))
+
+## spatial connectivity matrix
+xy = cbind(dat$lon, dat$lat) # currently there are issues with how the csv file is loaded and read
+temp = as.matrix(dist(xy))
+M = 1 * (temp < 1e-10)
+
+## SEQTRACK! 
+mu1 = 0.00502/365
+dat$date = as.POSIXct(dat$date)
+results  = seqTrack(distances, rownames(dat), dat$date, prox.mat=M, mu=mu1, haplo.le = nbNucleotide)  
+
+## Genetic likelihood
+p = get.likelihood(results, mu=mu1, hapl.length=nbNucleotide)
+
+head(results)
+tail(results)
+range(results$weight, na.rm=TRUE)
+barplot(table(results$weight)/sum(!is.na(results$weight)), ylab="Frequency",
+        xlab="Mutations between inferred ancestor and descendent", col="orange")
