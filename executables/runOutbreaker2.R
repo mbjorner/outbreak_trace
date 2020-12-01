c19c45c960df1f41ac7088b22a3694baee1357ce
Marianne Bjorner <mbbjorner@gmail.com>
2020-12-01 10:36:54 -0600

    unzip phyML results

--- executables/runOutbreaker2.R
+++ executables/runOutbreaker2.R
@@ -4,18 +4,33 @@ library(outbreaker2)
 
 # load fake_outbreak data
 # is a dataset of 30 separate DNA sequences of length 10000
-fakeOutbreak = data(fake_outbreak)
+data(fake_outbreak)
+dat = fake_outbreak
 
 # data that can be used from this fake outbreak
 sample = as.matrix(fake_outbreak$sample) # dates of isolation
 dnaSeq = as.matrix(fake_outbreak$dna) # (fake) pathogen genome sequences
 onset = as.matrix(fake_outbreak$onset) # dates of onset
-w = fake_outbreak$w #empirical distribution of generation time
+w = fake_outbreak$w # empirical distribution of generation time
 ances = fake_outbreak$ances # true infectors (i.e. true ancestors)
 ctd = fake_outbreak$ctd # contact tracing data
 
 
-save(fake_outbreak, file=paste("fakeOutbreak", simIdx, "RData", sep="."))
+# save(fake_outbreak, file=paste("fakeOutbreak", ----, "RData", sep="."))
+
+out = outbreaker(data = list(dna = dat$dna, dates = dat$onset, w_dens = dat$w),
+                config = list(n_iter = 2e4, sample_every = 200))
+plot(out)
+as.data.frame(out)
+
+# requires loading simulated data into workspace (as found in outbreak_trace/datasets/)
+dat2 = mySim
+                                                                # w as found in dat
+out2 = outbreaker(data = list(dna = dat2$seq, dates = dat2$dates, w_dens = dat$w),
+                 config = list(n_iter = 2e4, sample_every = 200))
+plot(out2)
+as.data.frame(out2)
+
 
 
 
---
+++ results/PhyML/H1N1/README.md
@@ -0,0 +1,17 @@
+Results using the online implementation of [PhyML](http://www.atgc-montpellier.fr/phyml/). 
+
+This can also be accomplished with the [PhyML R implementation](https://www.rdocumentation.org/packages/ape/versions/5.4-1/topics/phymltest) which uses the ape package,
+
+This method **does not** make use of time-series data, and relies upon genetic differences.
+
+PhyML's integrated SMS (model selection) found GTR to be the best model for the data. GTR is the general time-reversible model.
+
+Visualizations:
+
+[HA](http://www.atgc-montpellier.fr/presto/index.php?tree=20201130-011320_Wv25/h1n1_ha_sequences_txt_alter_phy_phyml_tree.txt). 
+
+Link might be dead, depending on how long these are kept. No worries, though. Can re-run with file h1n1_ha_sequences.txt.alter.phy as found in H1N1 data.
+
+[NA](http://www.atgc-montpellier.fr/presto/index.php?tree=20201130-012900_Jg78/h1n1_na_sequences_txt_alter_phy_phyml_tree.txt)
+
+Link might be dead, depending on how long these are kept. No worries, though. Can re-run with file h1n1_na_sequences.txt.alter.phy as found in H1N1 data.
