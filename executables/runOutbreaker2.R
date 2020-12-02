e991a5f3c5443df6228756108520b03549dffe95
Marianne Bjorner <mbbjorner@gmail.com>
2020-12-02 17:40:19 -0600

    outbreaker2 results now where they should be

--- executables/runOutbreaker2.R
+++ executables/runOutbreaker2.R
@@ -21,16 +21,23 @@ ctd = fake_outbreak$ctd # contact tracing data
 out = outbreaker(data = list(dna = dat$dna, dates = dat$onset, w_dens = dat$w),
                 config = list(n_iter = 2e4, sample_every = 200))
 plot(out)
-as.data.frame(out)
+fake_outbreak_results = as.data.frame(out)
+fake_outbreak_table = summary(out)$tree
+
+write.csv(fake_outbreak_table, '~/Documents/GitHub/outbreak_trace/results/outbreaker2/fake_outbreak_outbreaker2_results.csv')
+
 
 # requires loading simulated data into workspace (as found in outbreak_trace/datasets/)
 dat2 = mySim
                                                                 # w as found in dat
 out2 = outbreaker(data = list(dna = dat2$seq, dates = dat2$dates, w_dens = dat$w),
-                 config = list(n_iter = 2e4, sample_every = 200))
+                 config = list(n_iter = 2e3, sample_every = 200))
 plot(out2)
-as.data.frame(out2)
+random_diffusion = as.data.frame(out2)
 
+random_diffusion_table = summary(out2)$tree
+
+write.csv(random_diffusion_table, '~/Documents/GitHub/outbreak_trace/results/outbreaker2/random_diffusion_outbreaker2_results.csv')
 
 
 
--- executables/runSeqTrackSimData.R
+++ executables/runSeqTrackSimData.R
@@ -21,14 +21,14 @@ nbNucleotide = ncol(as.matrix(fake_outbreak$dna))
 distances    = dist.dna(fake_outbreak$dna, model = "raw")*ncol(as.matrix(fake_outbreak$dna))
 distances    = round(as.matrix(distances))
 
-## spatial connectivity matrix
+## spatial connectivity matrix - no geographical information
 # xy = cbind(dat$lon, dat$lat)
 # temp = as.matrix(dist(xy))
 # M = 1 * (temp < 1e-10)
 
 ## SEQTRACK! 
 mu1 = 0.00502/365
-dat$date = as.POSIXct(fake_outbreak$onset, tz = "GMT") # origin must be supplied ???
+fake_outbreak$onset = as.POSIXct(as.Date(fake_outbreak$onset, origin = "1970-01-01"))
 fake_outbreak_results  = seqTrack(distances, rownames(fake_outbreak), fake_outbreak$onset, prox.mat= NULL, mu=mu1, haplo.le = nbNucleotide)  
 # saves inferred ancestry information as a dataframe of seqTrack class
 
@@ -38,7 +38,7 @@ write.csv(fake_outbreak_results, '~/Documents/GitHub/outbreak_trace/results/seqT
 ## Genetic likelihood
 p = get.likelihood(results, mu=mu1, haplo.length=nbNucleotide)
 
-g = plot(results, main = "SeqTrack reconstruction of the H1N1 outbreak")
+g = plot(fake_outbreak_results, main = "SeqTrack reconstruction of outbreaker2's fake_outbreak")
 mtext(side = 3, text = "read: no/few mutations; grey: many mutations") 
 
 head(results)
